6678c28c25962ffbcb714a1887b98909a391bb39
Brandon Collins <brandon@flatironschool.com>
2017-10-02 18:33:33 +0000

    automatically backed up by learn

--- spec/01_cli_spec.rb
+++ spec/01_cli_spec.rb
@@ -1,5 +1,5 @@
-describe './bin/play'
-  it 'evokes #play passing it `board` as an argument'
+describe './bin/play' do
+  it 'evokes #play passing it `board` as an argument' do
     allow($stdout).to receive(:puts)
 
     expect(self).to receive(:play).with(kind_of(Array))
